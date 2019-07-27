594169ecc6970233075da621efdc974b82096511
Guerchôm Doré <d.homger@gmail.com>
2019-07-27 13:23:36 +0200

    Seem to work

--- cube.js
+++ cube.js
@@ -4,12 +4,21 @@
 
 class cube{
     constructor(parameters){
-      this.paramerters = objectDefaultValue(parameters, CUBE_DEFAULT_PARAMETERS);
+      this.parameters = objectDefaultValue(parameters, CUBE_DEFAULT_PARAMETERS);
 
       this.makeCube();
       this.geometrySetup();
+      this.transitionDuration = this.parameters.transitionDuration;
+      this.smoothSizeChange = this.parameters.smoothSizeChange;
+      if(this.smoothSizeChange){
+        this.toggleSmoothSizeChange();
+      }
 
-      if(this.paramerters.returnCube)
+      this._depth(this.parameters.depth);
+      this._width(this.parameters.width);
+      this._height(this.parameters.height);
+
+      if(this.parameters.returnCube)
         return this.cube;
     }
 
@@ -17,8 +26,10 @@ class cube{
 
     makeCube(){
       this.cube = document.createElement("div");
+      this.cube.className = "cube";
+
       
-      this.faceNames = ["front","back","top","bottom","left","rigth"];
+      this.faceNames = ["front","back","top","bottom","left","right"];
       this.faceNames.forEach(function(faceName){
         this[faceName] = document.createElement("div");
         this[faceName].className =  "cube_" + faceName;
@@ -33,8 +44,10 @@ class cube{
       this.cube.style.boxSizing = "border-box";
       this.cube.style.margin = "0";
       this.cube.style.padding = "0";
+      
 
       this.faceNames.forEach(function(faceName){
+        
         this[faceName].style.position = "absolute";
         this[faceName].style.top = "0";
         this[faceName].style.left = "0";
@@ -42,25 +55,116 @@ class cube{
         this[faceName].style.boxSizing = "border-box";
         this[faceName].style.margin = "0";
         this[faceName].style.padding = "0";
+        this[faceName].style.transformOrigin = "0 0";
+
+        CUBE_GEOEMETRY_STYLE[faceName].forEach(function(styleArray){
+          this[faceName].style[styleArray[0]] = styleArray[1];
+          if(styleArray[0] === "transform"){
+            this[faceName]._defaultTranformValue = " " + styleArray[1] + " ";
+          }
+        }.bind(this));
+
       }.bind(this));
 
-      let cachName = ["front","back"];
-      this.front.style.width = "100%";
-      this.front.style.height = "100%";
+      
 
-      this.back.style.transform = "rotateY(180deg)";
-      this.back.style.width = "100%";
-      this.back.style.height = "100%";
+    }
+    _depth(size){
+      if(_sizeIsValid(size)){
+        this.depth = size;
+        this.cube.style.transformOrigin = "50% 50% calc(-1*"+this.depth+"/2)";
+        
+        this.back._depthTransformValue = " translateZ(" +this.depth+") ";
 
-      this.top.style.width = "100%";
+        this.top.style.height = this.depth;
+        this.top._depthTransformValue = " translateY(calc(-1*" +this.depth+")) ";
+        
+        this.bottom.style.height = this.depth;
+        this.bottom._depthTransformValue = "  ";
 
-      this.bottom.style.width = "100%";
+        this.left.style.width = this.depth;
+        this.left._depthTransformValue = " translateX(calc(-1*" +this.depth+")) ";
+        
+        this.right.style.width = this.depth;
+        this.right._depthTransformValue = " translateX(calc(0*" +this.depth+")) ";
 
+        this.applySizeChange();
+        return;
+      }
+      console.warn("SIZE is not valid");
+    }
+
+
+    _width(size){
+      if(_sizeIsValid(size)){
+        this.width = size;        
+        this.cube.style.width = this.width;
+        
+        return;
+      }
+      console.warn("SIZE is not valid");
+    }
+
+
+    _height(size){
+      if(_sizeIsValid(size)){
+        this.height = size;
+        this.cube.style.height = this.height;
+        
+        return;
+      }
+      console.warn("SIZE is not valid");
+    }
+
+    applySizeChange(){
+
+      this.back.style.transform = this.back._defaultTranformValue + 
+      this.back._depthTransformValue;
+
+      this.left.style.transform = this.left._defaultTranformValue + 
+      this.left._depthTransformValue;
+      
+      this.right.style.transform = this.right._defaultTranformValue + 
+      this.right._depthTransformValue;
+
+      this.top.style.transform = this.top._defaultTranformValue + 
+      this.top._depthTransformValue;
+      
+      this.bottom.style.transform = this.bottom._defaultTranformValue + 
+      this.bottom._depthTransformValue;
+    }
+    
+    toggleSmoothSizeChange(){
+
+      this.cube.style.transitionProperty = this.smoothSizeChange ? "width, height, transform" : "";
+
+      this.faceNames.forEach(function(faceName){
+
+        this[faceName].style.transitionProperty = this.smoothSizeChange ? "width, height, transform" : "";
+      }.bind(this));
+
+      this.smoothSizeChange = !this.smoothSizeChange;
+    }
+
+    set transitionDuration(transitionDuration){
+      this.cube.style.transitionDuration = transitionDuration;
+
+      this.faceNames.forEach(function(faceName){
+
+        this[faceName].style.transitionDuration = transitionDuration;
+      }.bind(this));
     }
 }
 
-const FACE_STYLE = {
-  front : [],
+const CUBE_GEOEMETRY_STYLE = {
+  front : [["width","100%"],["height","100%"]],
+  back : [["width","100%"],["height","100%"],["transform"," rotateY(180deg) translateX(-100%)"]],
+
+  top : [["width","100%"],["transform"," rotateX(90deg) "]],
+  bottom : [["top","100%"],["width","100%"],["transform"," rotateX(-90deg) "]],
+
+  left : [["height","100%"],["transform"," rotateY(-90deg) "]],
+  right : [["left","100%"],["height","100%"],["transform"," rotateY(90deg) "]],
 };
 
 
@@ -87,6 +191,13 @@ const CUBE_DEFAULT_PARAMETERS = {
     height: "50px",
     depth: "50px",
     returnCube : true,
+    smoothSizeChange: true,
+    transitionDuration: "1s",
+
 }
 Object.freeze(CUBE_DEFAULT_PARAMETERS);
 
+function _sizeIsValid(size){
+
+  return true;
+}
--- script.js
+++ script.js
@@ -1,10 +1,112 @@
 'use strict';
+const name = ["front","back","top","bottom","left","right"];
+const srcArray = [`<iframe width="560" height="315" src="https://www.youtube.com/embed/YYlYW3i9HxQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`];
 
 
 document.onreadystatechange = function(){
     if(document.readyState === "complete"){
         let main = document.querySelector("main");
 
-        main.append(new cube());
+        let _cube = new cube({returnCube: false, smoothSizeChange: true, transitionDuration: "8s"})
+        main.append(_cube.cube);
+
+        document.querySelectorAll(".cube > *").forEach(function(element){
+          //rondomColor(element);
+          element.innerHTML = srcArray[ 
+            Math.floor(Math.random() * srcArray.length)
+           ];//"_______" + element.className;
+          let iframe = document.createElement("iframe");
+          iframe.style.height = "100%";
+          iframe.style.width = "100%";
+          iframe.src = srcArray[ 
+            Math.floor(Math.random() * srcArray.length)
+           ];
+          //element.append(iframe);
+
+
+        });
+
+
+        let h = document.getElementById("h");
+        h.onkeyup = function(){
+          _cube._height(this.value + "px");
+        }
+        _cube._height(h.value + "px");
+        
+        let w = document.getElementById("w");
+        w.onkeyup = function(){
+          _cube._width(this.value + "px");
+        }
+        _cube._width(w.value + "px");
+
+        let d = document.getElementById("d");
+        d.onkeyup = function(){
+          _cube._depth(this.value + "px");
+        }
+        _cube._depth(d.value + "px");
+        
+
+        let x = document.getElementById("x");
+        x.onkeyup = function(){
+          X = 0;
+          rotateX = " rotateX( " + this.value+ "deg ) ";
+          _cube.cube.style.transform = rotateX + rotateY + rotateZ;
+        }
+
+        let y = document.getElementById("y");
+        y.onkeyup = function(){
+          Y = 0;
+          rotateY = " rotateY( " + this.value + "deg ) ";
+          _cube.cube.style.transform = rotateX + rotateY + rotateZ;
+        }
+        let z = document.getElementById("z");
+        z.onkeyup = function(){
+          Z = 0;
+          rotateZ = " rotateZ( " + this.value + "deg ) ";
+          _cube.cube.style.transform = rotateX + rotateY + rotateZ;
+        }
+
+
+        rotateX = " rotateX( " + X + "deg ) ";
+        rotateY = " rotateX( " + Y + "deg ) ";
+        rotateZ = " rotateX( " + Z + "deg ) ";
+        
+        _cube.cube.style.transform = rotateX + rotateY + rotateZ;
+
+
+
+        document.getElementById("bX").onclick = function(){
+          ++X;
+          rotateX = " rotateX( " + X*90+ "deg ) ";
+          _cube.cube.style.transform = rotateX + rotateY + rotateZ;
+        }
+        document.getElementById("bY").onclick = function(){
+          ++Y;
+          rotateY = " rotateY( " + Y*90+ "deg ) ";
+          _cube.cube.style.transform = rotateX + rotateY + rotateZ;
+        }
+        document.getElementById("bZ").onclick = function(){
+          ++Z;
+          rotateZ = " rotateZ( " + Z*90+ "deg ) ";
+          _cube.cube.style.transform = rotateX + rotateY + rotateZ;
+        }
     }
+}
+
+var X = 0, Y = 0, Z = 0;
+
+var rotateX = "0deg";
+var rotateY = "0deg";
+var rotateZ = "0deg";
+
+function rondomColor(element){
+  element.style.backgroundColor = getRandomColor();
+}
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
